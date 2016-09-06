4a1b3acafa6a7d9bed3101ad4feb731f67488447
Sam Fries <fries2@llnl.gov>
2016-09-06 13:41:03 -0700

    Can load basic NC variable data into cesium

--- app/main.py
+++ app/main.py
@@ -3,6 +3,21 @@
 import tornado.web
 import tornado.ioloop
 import os
+import vcs
+import cdms2
+import numpy
+
+
+def write_array(array, handler):
+    """
+    Writes numpy array to response with headers that CDMS.js expects.
+    """
+    shape = array.shape
+    dtype = array.dtype
+
+    handler.add_header("x-cdms-datatype", dtype.name)
+    handler.add_header("x-cdms-shape", ",".join((str(s) for s in shape)))
+    handler.write(array.flatten().tobytes())
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -15,11 +30,67 @@ class MainHandler(tornado.web.RequestHandler):
             self.write(ind.read())
 
 
+class NCMetaHandler(tornado.web.RequestHandler):
+    """Retrieves grid info for a nc file."""
+
+    def get(self):
+        filename = self.get_argument("file", default="clt.nc")
+        # Ignore arbitrary paths, just check if the file exists
+        filename = os.path.basename(filename)
+        varname = self.get_argument("variable", default=None)
+        if filename == "clt.nc" and varname is None:
+            varname = "clt"
+        try:
+            f = cdms2.open(os.path.join(vcs.sample_data, filename))
+            s = f(varname)
+            g = s.getGrid()
+            # number of cells, lat/lon, number of points per cel
+            m = g.getMesh()
+            num_points_per_cell = m.shape[2]
+            # So we need to reshape it to have a flat array
+            # We'll reshape to look like this:
+            # [ [[lon, lat], [lon, lat], ...], [[lon, lat], [lon, lat], ...], ...]
+
+            # First, flip lat and lon arrays
+            flipped = numpy.fliplr(m)
+            # Then transpose into lon/lat pairs
+            transposed = numpy.transpose(flipped, [0, 2, 1])
+
+            write_array(transposed, self)
+        except:
+            self.write("Failed to find file/variable.")
+
+
+class NCDataHandler(tornado.web.RequestHandler):
+    """Retrieves data for cells in a nc file."""
+
+    def get(self):
+        filename = self.get_argument("file", default="clt.nc")
+        # Ignore arbitrary paths, just check if the file exists
+        filename = os.path.basename(filename)
+        varname = self.get_argument("variable", default=None)
+        if filename == "clt.nc" and varname is None:
+            varname = "clt"
+        timeslice = self.get_argument("timeslice", default=0)
+
+        try:
+            f = cdms2.open(os.path.join(vcs.sample_data, filename))
+            s = f(varname)
+            write_array(s[int(timeslice)], self)
+        except:
+            self.write("Failed to find file/variable/time")
+
+
 def make_app():
     """Generate WSGI app."""
     return tornado.web.Application([
-        (r"/", MainHandler),
-    ], static_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "static"), static_url_prefix="/static/")
+            (r"/", MainHandler),
+            (r"/data/meta", NCMetaHandler),
+            (r"/data", NCDataHandler)
+        ],
+        static_path=os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                 "static"),
+        static_url_prefix="/static/")
 
 if __name__ == "__main__":
     app = make_app()
